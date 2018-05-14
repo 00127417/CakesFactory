a27935838cf47601fb82df46b6c0f7e063be0be0
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-05-14 08:49:50 -0600

    Se creo la clase enum CakeType

---
+++ cakesFactory/src/cakesfactory/CakeType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cakesfactory;
+
+/**
+ *
+ * @author Oscar Lovato
+ */
+public enum CakeType {
+    CHOCOLATE,FRUTAS,CARAMELO;
+}
